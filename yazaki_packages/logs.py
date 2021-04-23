1d083ba1bc7875421249182d137e213be49aa124
Taweechai Yuenyang <krumii.it@gmail.com>
2021-04-23 10:21:11 +0700

    updatenew mkdir

--- yazaki_packages/logs.py
+++ yazaki_packages/logs.py
@@ -8,6 +8,9 @@ class Logging():
         import os
         import pathlib
 
+        if os.path.exists(f'{pathlib.Path().absolute()}/logs') is False:
+            os.makedirs(f'{pathlib.Path().absolute()}/logs')
+
         logfilename = f"{pathlib.Path().absolute()}/logs/{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
         f = open(logfilename, mode='a+')
         f.writelines(f"{datetime.datetime.now().strftime('%Y-%m-%d %X')} {args[0]} {args[1]} {args[2]}\n")
