d808483f2ea3b0688a22bce7bb55fa118d22c6a4
Taweechai Yuenyang <krumii.it@gmail.com>
2021-04-23 10:16:40 +0700

    update new logs

--- yazaki_packages/logs.py
+++ yazaki_packages/logs.py
@@ -6,8 +6,9 @@ class Logging():
     def __init__(self, *args, **kwargs):
         import datetime
         import os
+        import pathlib
 
-        logfilename = f"logs/{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
+        logfilename = f"{pathlib.Path().absolute()}/logs/{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
         f = open(logfilename, mode='a+')
         f.writelines(f"{datetime.datetime.now().strftime('%Y-%m-%d %X')} {args[0]} {args[1]} {args[2]}\n")
         f.close()
