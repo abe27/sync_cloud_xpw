798c27b55a428b6e09ccae0191bdfe4e887c1cc9
Taweechai Yuenyang <taweechai@seiwa-pioneer.com>
2021-05-12 08:32:09 +0700

    update test new log file

--- yazaki_packages/logs.py
+++ yazaki_packages/logs.py
@@ -8,17 +8,17 @@ class Logging():
         import os
         import pathlib
 
-        if os.path.exists(f'{pathlib.Path().absolute()}/GEDI/logs') is False:
-            os.makedirs(f'{pathlib.Path().absolute()}/GEDI/logs')
+        if os.path.exists(f'{pathlib.Path().absolute()}/GEDI/LOGS') is False:
+            os.makedirs(f'{pathlib.Path().absolute()}/GEDI/LOGS')
 
-        logfilename = f"{pathlib.Path().absolute()}/GEDI/logs/{datetime.datetime.now().strftime('%Y-%m-%d')}.log"
+        logfilename = f"{pathlib.Path().absolute()}/GEDI/LOGS/{datetime.datetime.now().strftime('%Y%m%d')}.log"
         if os.path.exists(logfilename) is False:
             f = open(logfilename, mode='a+')
-            f.writelines(f"{'0.'.ljust(5)} {str(datetime.datetime.now().strftime('%Y-%m-%d %X')).ljust(10)} {str(args[0]).ljust(5)}  {str(args[1]).ljust(90)} {(str(args[2]).lower()).ljust(5)}\n")
+            f.writelines(f"{'0.'.ljust(5)} {str(datetime.datetime.now().strftime('%Y-%m-%d %X')).ljust(10)} {str(args[0]).ljust(5)}  {str(args[1]).ljust(70)} {(str(args[2]).lower()).ljust(5)}\n")
             f.close()
 
         else:
             lines = open(logfilename, mode='r')
             f = open(logfilename, mode='a+')
-            f.writelines(f"{(str(len(lines.readlines()))+'.').ljust(5)} {str(datetime.datetime.now().strftime('%Y-%m-%d %X')).ljust(10)} {str(args[0]).ljust(5)}  {str(args[1]).ljust(90)} {(str(args[2]).lower()).ljust(5)}\n")
+            f.writelines(f"{(str(len(lines.readlines()))+'.').ljust(5)} {str(datetime.datetime.now().strftime('%Y-%m-%d %X')).ljust(10)} {str(args[0]).ljust(5)}  {str(args[1]).ljust(70)} {(str(args[2]).lower()).ljust(5)}\n")
             f.close()
