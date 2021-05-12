f95380a4dd3ead2ec3e54b933c2cd749917cdafe
Taweechai Yuenyang <taweechai@seiwa-pioneer.com>
2021-05-12 08:40:35 +0700

    update writing log with line number

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+import datetime
 from yazaki_packages.logs import Logging
 t = Logging("title", "description", "STATUS")
+
+etd = str((datetime.datetime.now() - datetime.timedelta(days=0)).strftime('%Y%m%d'))
+print(etd)
 
--- yazaki_packages/logs.py
+++ yazaki_packages/logs.py
@@ -12,13 +12,11 @@ class Logging():
             os.makedirs(f'{pathlib.Path().absolute()}/GEDI/LOGS')
 
         logfilename = f"{pathlib.Path().absolute()}/GEDI/LOGS/{datetime.datetime.now().strftime('%Y%m%d')}.log"
-        if os.path.exists(logfilename) is False:
-            f = open(logfilename, mode='a+')
-            f.writelines(f"{'0.'.ljust(5)} {str(datetime.datetime.now().strftime('%Y-%m-%d %X')).ljust(10)} {str(args[0]).ljust(5)}  {str(args[1]).ljust(70)} {(str(args[2]).lower()).ljust(5)}\n")
-            f.close()
-
-        else:
+        l = 1
+        if os.path.exists(logfilename):
             lines = open(logfilename, mode='r')
-            f = open(logfilename, mode='a+')
-            f.writelines(f"{(str(len(lines.readlines()))+'.').ljust(5)} {str(datetime.datetime.now().strftime('%Y-%m-%d %X')).ljust(10)} {str(args[0]).ljust(5)}  {str(args[1]).ljust(70)} {(str(args[2]).lower()).ljust(5)}\n")
-            f.close()
+            l = len(lines.readlines()) + 1
+
+        f = open(logfilename, mode='a+')
+        f.writelines(f"{(str(l)+'.').ljust(5)} {str(datetime.datetime.now().strftime('%Y-%m-%d %X')).ljust(10)} {str(args[0]).ljust(5)}  {str(args[1]).ljust(70)} {(str(args[2]).lower()).ljust(5)}\n")
+        f.close()
