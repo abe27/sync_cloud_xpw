e163dab4aae7d9789391628bbb835de30cad0b44
Taweechai Yuenyang <taweechai@seiwa-pioneer.com>
2021-05-14 14:47:39 +0700

    update test chacng status

--- load_test.py
+++ load_test.py
@@ -7,7 +7,17 @@ def main():
     for i in doc:
         filename = str(i[1])
         _date = datetime.datetime.strptime(filename[17:(25+6)], "%Y%m%d%H%M%S") #OES.VCBI.32T5.SPL20210422203000.TXT
-        sql_update = f"update tbt_gedi_datas set download=0,is_download_from_cloud=true,upload_date='{str(_date)}',upload_time='{str(_date)}',created_at='{str(_date)}',updated_at='{str(_date)}' where id='{i[0]}'"
+        sql_update = f"""update tbt_gedi_datas 
+        set 
+        download=0,
+        is_download_from_cloud=true,
+        upload_date='{str(_date)}',
+        upload_time='{str(_date)}',
+        created_at='{str(_date)}',
+        updated_at='{str(_date)}',
+        user_id='65d0bc07-8ce4-4eed-8abd-a25bf389301b',
+        gedi_type_id='711b7af0-02f7-4437-b59b-c6d09c74da1b'
+        where id='{i[0]}'"""
         PsDb().excute_data(sql_update)
         print(f"update {i[0]}")
 
