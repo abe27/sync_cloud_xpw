927b50aa18badc88acd822d19701b02ae10191a5
Taweechai Yuenyang <taweechai@seiwa-pioneer.com>
2021-05-20 15:11:20 +0700

    move file

--- read_pdf.py
+++ read_pdf.py
@@ -2,7 +2,25 @@ import io
 import sys
 import os
 import PyPDF4
+import datetime
 from yazaki_packages.cloud import SplCloud
+from yazaki_packages.db import OraFG
+
+
+def insert_db(obj):
+    for r in obj:
+        sql = f"""INSERT INTO TMP_ORDER(ITEM, PDS_NO, FROM_ETD_DATE, FROM_ETD_TIME, TO_ETD_DATE, TO_ETD_TIME, DEST_NAME, NAME_001, COMPANY_NAME, FROM_TAP, TAP_ROUND, PL_LIMIT, PL_NO, GROUP_NO, PAGE_NO, ACC_NO, DELIVERY_FROM_DATE, DELIVERY_FROM_TIME, DELIVERY_TO_DATE, DELIVERY_TO_TIME, PARTNO, TAG_CODE, TAG_NAME, STDPACK, CT, QTY,FILE_NAME)
+        VALUES({r['no']}, '{r['pds_no']}',to_date('{r['to_etd_date']}', 'DD/MM/YYYY'),'{r['to_etd_time']}',to_date('{r['from_etd_date']}', 'DD/MM/YYYY'),'{r['from_etd_time']}','{r['dest_name']}','{r['name_001']}','{r['comany_name']}','{r['from_tap']}','{r['tap_round']}','{r['pl_limit']}','{r['pl_no']}','{r['group_no']}','{r['page_no']}','{r['acc_no']}',to_date('{r['delivery_from_date']}', 'DD/MM/YY'),'{r['delivery_from_time']}',to_date('{r['delivery_to_date']}', 'DD/MM/YY'),'{r['delivery_to_time']}','{r['part_no']}','{r['part_tag_code']}','{r['part_tag_name']}','{r['part_stdpack']}','{r['part_ctn']}','{r['part_qty']}','{r['file_name']}')"""
+        if OraFG().get_fetch_one(f"select ITEM from TMP_ORDER where PDS_NO='{r['pds_no']}' and PARTNO='{r['part_no']}'") > 0:
+           sql = f"""UPDATE TMP_ORDER SET CT='{r['part_ctn']}', QTY='{r['part_qty']}', SYNC=0 WHERE PDS_NO='{r['pds_no']}' and PARTNO='{r['part_no']}'"""
+        
+        OraFG().excute_data(sql)
+
+def move_to_home(source_file, filename):
+    print(os.getenv("HOME"))
+    fname = os.getenv("HOME") + f"/GEDI/PDF/{datetime.datetime.now().strftime('%Y%m%d')}/{filename}"
+    if os.path.exists(fname):
+        os.remove(fname)
 
 def main():
     fname = os.listdir("./pdf")
@@ -15,63 +33,65 @@ def main():
                 page = pdfReader.getPage(j)
                 data = io.StringIO(page.extractText())
 
-                pds_no              = None
-                to_etd_date         = None
-                to_etd_time         = None
-                from_etd_date       = None
-                from_etd_time       = None
-                dest_name           = None
-                name_001            = None
-                comany_name         = None
-                from_tap            = None
-                tap_round           = None
-                pl_limit            = None
-                pl_no               = None
-                group_no            = None
-                page_no             = None
-                acc_no              = None
-                delivery_from_date  = None
-                delivery_from_time  = None
-                delivery_to_date    = None
-                delivery_to_time    = None
+                pds_no = None
+                to_etd_date = None
+                to_etd_time = None
+                from_etd_date = None
+                from_etd_time = None
+                dest_name = None
+                name_001 = None
+                comany_name = None
+                from_tap = None
+                tap_round = None
+                pl_limit = None
+                pl_no = None
+                group_no = None
+                page_no = None
+                acc_no = None
+                delivery_from_date = None
+                delivery_from_time = None
+                delivery_to_date = None
+                delivery_to_time = None
 
-                part_no             = None
-                part_tag_code       = None
-                part_tag_name       = None
-                part_stdpack        = None
-                part_ctn            = None
-                part_qty            = None
+                part_no = None
+                part_tag_code = None
+                part_tag_name = None
+                part_stdpack = None
+                part_ctn = None
+                part_qty = None
 
                 doc = []
 
-                if os.path.exists(f"./temp/{i.replace('pdf', 'txt')}"):
-                    os.remove(f"./temp/{i.replace('pdf', 'txt')}")
+                # if os.path.exists(f"./temp/{i.replace('pdf', 'txt')}"):
+                #     os.remove(f"./temp/{i.replace('pdf', 'txt')}")
 
-                f = open(f"./temp/{i.replace('pdf', 'txt')}", "a+")
+                # f = open(f"./temp/{i.replace('pdf', 'txt')}", "a+")
 
+                num_line = 67
                 j = 0
                 x = 0
                 for r in data:
                     r = str(r).strip()
-                    txt = (f"{str(str(x) + '.').ljust(5)} => {str(len(r)).ljust(5)} ::: {r}\n")
+                    txt = (
+                        f"{str(str(x) + '.').ljust(5)} => {str(len(r)).ljust(5)} ::: {r}\n")
                     if x == 0:
                         pds_no = r
 
                     if x == 1:
                         to_etd_date = r
-                    
+
                     if x == 2:
                         to_etd_time = r
 
                     if x == 3:
                         from_etd_date = r
-                    
+
                     if x == 4:
                         from_etd_time = r
 
                     if x == 5:
                         dest_name = r
-                    
+
                     if x == 6:
                         name_001 = r
 
@@ -83,10 +103,10 @@ def main():
 
                     if x == 10:
                         tap_round = r
-                    
+
                     if x == 11:
                         pl_limit = r
-                    
+
                     if x == 12:
                         pl_no = r
 
@@ -100,21 +120,31 @@ def main():
                         acc_no = r
 
                     if x == 29:
-                        delivery_from_date  = r
+                        delivery_from_date = r
 
                     if x == 30:
-                        delivery_from_time  = r
+                        delivery_from_time = r
 
                     if x == 31:
-                        delivery_to_date    = r
+                        delivery_to_date = r
 
                     if x == 32:
-                        delivery_to_time    = r
+                        delivery_to_time = r
 
-                    elif x > 67:
+                    if x == 33:
+                        if r.find("REPRINT") < 0:
+                            num_line = 65
+
+                    # fixed_part = True
+                    # if len(r) == 18:
+                    #     fixed_part = False
+
+                    if x > num_line:
                         if r.find('TOTAL') < 0:
                             if j == 0:
                                 part_no = r
+                                if len(r) == 18:
+                                    part_no = r
 
                             if j == 1:
                                 part_tag_code = r
@@ -135,49 +165,51 @@ def main():
                             if j >= 6:
                                 doc.append({
                                     "no": len(doc) + 1,
-                                    "pds_no":pds_no,
-                                    "to_etd_date":to_etd_date,
-                                    "to_etd_time":to_etd_time,
-                                    "from_etd_date":from_etd_date,
-                                    "from_etd_time":from_etd_time,
-                                    "dest_name":dest_name,
-                                    "name_001":name_001,
-                                    "comany_name":comany_name,
-                                    "from_tap":from_tap,
-                                    "tap_round":tap_round,
-                                    "pl_limit":pl_limit,
-                                    "pl_no":pl_no,
-                                    "group_no":group_no,
-                                    "page_no":page_no,
-                                    "acc_no":acc_no,
-                                    "delivery_from_date":delivery_from_date,
-                                    "delivery_from_time":delivery_from_time,
-                                    "delivery_to_date":delivery_to_date,
-                                    "delivery_to_time":delivery_to_time,
+                                    "pds_no": pds_no,
+                                    "to_etd_date": to_etd_date,
+                                    "to_etd_time": to_etd_time,
+                                    "from_etd_date": from_etd_date,
+                                    "from_etd_time": from_etd_time,
+                                    "dest_name": dest_name,
+                                    "name_001": name_001,
+                                    "comany_name": comany_name,
+                                    "from_tap": from_tap,
+                                    "tap_round": tap_round,
+                                    "pl_limit": pl_limit,
+                                    "pl_no": pl_no,
+                                    "group_no": group_no,
+                                    "page_no": page_no,
+                                    "acc_no": acc_no,
+                                    "delivery_from_date": delivery_from_date,
+                                    "delivery_from_time": delivery_from_time,
+                                    "delivery_to_date": delivery_to_date,
+                                    "delivery_to_time": delivery_to_time,
                                     "part_no": part_no,
                                     "part_tag_code": part_tag_code,
                                     "part_tag_name": part_tag_name,
                                     "part_stdpack": part_stdpack,
                                     "part_ctn": part_ctn,
                                     "part_qty": part_qty,
+                                    "file_name": i,
                                 })
                                 j = 0
-                                part_no             = None
-                                part_tag_code       = None
-                                part_tag_name       = None
-                                part_stdpack        = None
-                                part_ctn            = None
-                                part_qty            = None
-                    
+                                part_no = None
+                                part_tag_code = None
+                                part_tag_name = None
+                                part_stdpack = None
+                                part_ctn = None
+                                part_qty = None
 
-
-                    f.write(txt)
+                    # f.write(txt)
+                    print(txt)
                     x += 1
 
-                f.close()
+                # f.close()
                 print(f"========= {i} ========")
-                print(list(doc))
-                
+                insert_db(doc)
+                move_to_home(f"./pdf/{i}", i)
+
+
 if __name__ == '__main__':
     main()
     sys.exit(0)
--- yazaki_packages/db.py
+++ yazaki_packages/db.py
@@ -140,6 +140,74 @@ class OraDB:
         conn.close()
         return f
 
+class OraFG:
+    def __init__(self, *args, **kwargs):
+        return None
+
+    def get_fetch_one(self, sql):
+        import os
+        import cx_Oracle
+        from yazaki_packages.logs import DBLogging
+
+        conn = cx_Oracle.connect(os.getenv("ORA_FG"))
+        i = 0
+        cur = conn.cursor()
+        try:
+            cur.execute(sql)
+            db = cur.fetchone()
+            # print(type(db))
+            if db != None:
+                i = db[0]
+            else:
+                i = False
+
+        except Exception as e:
+            print(sql)
+            print(str(e))
+            DBLogging("ORA_FG" , f"ERROR FETCH ONE", str(e))
+        
+        cur.close()
+        return i
+
+    def get_fetch_all(self, sql):
+        import os
+        import cx_Oracle
+        from yazaki_packages.logs import DBLogging
+
+        try:
+            conn = cx_Oracle.connect(os.getenv("ORA_FG"))
+            cur = conn.cursor()
+            cur.execute(sql)
+            obj = cur.fetchall()
+        except Exception as ex:
+            print(ex)
+            DBLogging("ORA_FG" , f"ERROR FETCH ALL", str(ex))
+            pass
+        return obj
+
+    def excute_data(self, sql):
+        import os
+        import cx_Oracle
+        from yazaki_packages.logs import DBLogging
+
+        f = True
+        conn = cx_Oracle.connect(os.getenv("ORA_FG"))
+        cur = conn.cursor()
+        try:
+            cur.execute(sql)
+            conn.commit()
+            pass
+        except Exception as e:
+            print(sql)
+            print(str(e))
+            f = False
+            DBLogging("ORA_FG" , f"ERROR EXCUTE", str(e))
+            conn.rollback()
+            pass
+
+        cur.close()
+        conn.close()
+        return f
 
 class WmsDb:
     def __init__(self, *args, **kwargs):
