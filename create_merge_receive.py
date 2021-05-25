881a0634ea55ccaa48e1577d782f1bbd19b75b76
Taweechai Yuenyang <taweechai@seiwa-pioneer.com>
2021-05-25 08:05:25 +0700

    remove duplicate receive merge

--- create_merge_receive.py
+++ create_merge_receive.py
@@ -37,36 +37,35 @@ def main():
         sql_body = f"""SELECT '{str(i[2]).strip()}' RECENO, PARTNO,sum(PLNQTY) qty,sum(PLNCTN) ctn,UNIT,CD,WHS,DESCRI FROM TXP_RECTRANSBODY WHERE RECEIVINGKEY IN ({receive_key}) GROUP BY PARTNO,UNIT,CD,WHS,DESCRI ORDER BY PARTNO"""
         cur.execute(sql_body)
         body = cur.fetchall()
+        if len(body) > 0:
+            plnctn = 0
+            rec_tag = ""
+            x = 1
+            for j in body:
+                rvno = OraDB().get_fetch_one(f"(select 'BD'|| TO_CHAR(sysdate,'yyMMdd') || replace(to_char(emp_TXP__RCMANGENO_CK2.nextval,'00099'),' ','') as genrunno  from dual)")
+                print(f"{rvno} => {list(j)}")
+                partname = str(j[7]).replace("'", "''")
+                sql_rec_body = (f"""INSERT INTO TXP_RECTRANSBODY
+                                    (RECEIVINGKEY, RECEIVINGSEQ, PARTNO, PLNQTY, PLNCTN,RECQTY,RECCTN,TAGRP, UNIT, CD, WHS, DESCRI, RVMANAGINGNO,UPDDTE, SYSDTE, CREATEDBY,MODIFIEDBY,OLDERKEY)
+                                    VALUES('{j[0]}', '{x}', '{j[1]}', {j[2]}, {j[3]},0,0,'C', '{j[4]}','{j[5]}' , '{j[6]}','{partname}', '{rvno}',sysdate, sysdate, 'SKTSYS', 'SKTSYS', '{xkeys}')""")
+                plnctn += int(str(j[3]))
+                rec_tag = str(j[6])
+                cur.execute(sql_rec_body)
+                x += 1
 
-        plnctn = 0
-        rec_tag = ""
-        x = 1
-        for j in body:
-            rvno = OraDB().get_fetch_one(f"(select 'BD'|| TO_CHAR(sysdate,'yyMMdd') || replace(to_char(emp_TXP__RCMANGENO_CK2.nextval,'00099'),' ','') as genrunno  from dual)")
-            print(f"{rvno} => {list(j)}")
-            partname = str(j[7]).replace("'", "''")
-            sql_rec_body = (f"""INSERT INTO TXP_RECTRANSBODY
-                                (RECEIVINGKEY, RECEIVINGSEQ, PARTNO, PLNQTY, PLNCTN,RECQTY,RECCTN,TAGRP, UNIT, CD, WHS, DESCRI, RVMANAGINGNO,UPDDTE, SYSDTE, CREATEDBY,MODIFIEDBY,OLDERKEY)
-                                VALUES('{j[0]}', '{x}', '{j[1]}', {j[2]}, {j[3]},0,0,'C', '{j[4]}','{j[5]}' , '{j[6]}','{partname}', '{rvno}',sysdate, sysdate, 'SKTSYS', 'SKTSYS', '{xkeys}')""")
-            plnctn += int(str(j[3]))
-            rec_tag = str(j[6])
-            cur.execute(sql_rec_body)
-            x += 1
+            sql_insert_ent = f"""INSERT INTO TXP_RECTRANSENT(RECEIVINGKEY, RECEIVINGMAX, RECEIVINGDTE, VENDOR, RECSTATUS, RECISSTYPE, RECPLNCTN,RECENDCTN, UPDDTE, SYSDTE)
+            VALUES('{str(i[2]).strip()}', {len(body)}, to_date('{datetime.now().strftime('%Y-%m-%d')}', 'YYYY-MM-DD'), '{rec_tag}', 0, '01', '{plnctn}',0, current_timestamp, current_timestamp)"""
+            cur.execute(sql_insert_ent)
 
-        sql_insert_ent = f"""INSERT INTO TXP_RECTRANSENT(RECEIVINGKEY, RECEIVINGMAX, RECEIVINGDTE, VENDOR, RECSTATUS, RECISSTYPE, RECPLNCTN,RECENDCTN, UPDDTE, SYSDTE)
-        VALUES('{str(i[2]).strip()}', {len(body)}, to_date('{datetime.now().strftime('%Y-%m-%d')}', 'YYYY-MM-DD'), '{rec_tag}', 0, '01', '{plnctn}',0, current_timestamp, current_timestamp)"""
-        cur.execute(sql_insert_ent)
-
-        # delete after merge
-        cur.execute(f"DELETE TXP_RECTRANSBODY WHERE RECEIVINGKEY IN ({receive_key})")
-        cur.execute(f"DELETE TXP_RECTRANSENT WHERE RECEIVINGKEY IN ({receive_key})")
+            # delete after merge
+            cur.execute(f"DELETE TXP_RECTRANSBODY WHERE RECEIVINGKEY IN ({receive_key})")
+            cur.execute(f"DELETE TXP_RECTRANSENT WHERE RECEIVINGKEY IN ({receive_key})")
 
         ## delete temp data
         cur.execute(f"DELETE TMP_RECEIVEMERGE WHERE ID='{str(i[0])}'")
         print(f"DELETE TMP_RECEIVEMERGE ID => '{str(i[0])}'")
         ora.commit()
-
-        if len(body) > 0:
+        if len(doc) > 0:
             msg = f"MERGE RECEIVE({rec_tag})\nRECEIVENO: {str(i[2]).strip()}\nITEM: {len(body)} CTN: {plnctn}\nFROM: {xkeys}\nAT: {datetime.now().strftime('%Y-%m-%d %X')}"
             SplCloud().linenotify(msg)
 
